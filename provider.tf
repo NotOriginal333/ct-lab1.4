4fd8631b884a202d6f7e70a996dd738f805e4f93
NotOriginal333 <m.naumovych2@gmail.com>
2025-04-01 11:11:00 +0300

    done lab1

--- provider.tf
+++ provider.tf
@@ -1,9 +1,6 @@
 terraform {
-  backend "s3" {
-    bucket         = "657694663228-2025-terraform-tfstate"
-    key            = "dev/terraform.tfstate"
-    region         = "eu-central-1"
-    dynamodb_table = "657694663228-2025-terraform-tfstate-lock"
+   backend "local" {
+    path = "./terraform.tfstate"
   }
 
   required_providers {
