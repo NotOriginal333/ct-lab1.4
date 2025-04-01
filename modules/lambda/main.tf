b1df2ec41a5a52cc0125c4dcaeb3717549e0af84
NotOriginal333 <m.naumovych2@gmail.com>
2025-04-01 10:23:48 +0300

    added new lambdas

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -14,6 +14,22 @@ module "label_get_all_authors" {
   name    = "get-all-authors"
 }
 
+module "label_get_all_courses" {
+  source = "cloudposse/label/null"
+  # Cloud Posse recommends pinning every module to a specific version
+  version = "0.25.0"
+  context = var.context
+  name    = "get-all-courses"
+}
+
+module "label_get_course" {
+  source = "cloudposse/label/null"
+  # Cloud Posse recommends pinning every module to a specific version
+  version = "0.25.0"
+  context = var.context
+  name    = "get-course"
+}
+
 module "label_save_course" {
   source = "cloudposse/label/null"
   # Cloud Posse recommends pinning every module to a specific version
@@ -47,6 +63,59 @@ module "lambda_get_all_authors" {
 
   tags = module.label.tags
 }
+
+module "lambda_get_all_courses" {
+  source        = "terraform-aws-modules/lambda/aws"
+  version       = "7.20.1"
+  function_name = module.label_get_all_courses.id
+  description   = "My awesome lambda function"
+  handler       = "index.handler"
+  runtime       = "nodejs16.x"
+
+  source_path = "${path.module}/src/get-all-courses"
+
+  environment_variables = {
+    TABLE_NAME = var.courses_table
+  }
+  attach_policy_statements = true
+
+  policy_statements = {
+    dynamodb = {
+      effect    = "Allow",
+      actions   = ["dynamodb:Scan"],
+      resources = ["${var.courses_table_arn}"]
+    }
+  }
+
+  tags = module.label.tags
+}
+
+module "lambda_get_course" {
+  source        = "terraform-aws-modules/lambda/aws"
+  version       = "7.20.1"
+  function_name = module.label_get_course.id
+  description   = "My awesome lambda function"
+  handler       = "index.handler"
+  runtime       = "nodejs16.x"
+
+  source_path = "${path.module}/src/get-course"
+
+  environment_variables = {
+    TABLE_NAME = var.courses_table
+  }
+  attach_policy_statements = true
+
+  policy_statements = {
+    dynamodb = {
+      effect    = "Allow",
+      actions   = ["dynamodb:GetItem"],
+      resources = ["${var.courses_table_arn}"]
+    }
+  }
+
+  tags = module.label.tags
+}
+
 
 module "lambda_save_course" {
   source        = "terraform-aws-modules/lambda/aws"
