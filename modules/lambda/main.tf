eaea8fba270b142c07d88981c4d3dcb970291369
NotOriginal333 <m.naumovych2@gmail.com>
2025-04-01 15:15:58 +0300

    lab1 2 more lambda

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -30,6 +30,22 @@ module "label_get_course" {
   name    = "get-course"
 }
 
+module "label_update_course" {
+  source = "cloudposse/label/null"
+  # Cloud Posse recommends pinning every module to a specific version
+  version = "0.25.0"
+  context = var.context
+  name    = "update-course"
+}
+
+module "label_delete_course" {
+  source = "cloudposse/label/null"
+  # Cloud Posse recommends pinning every module to a specific version
+  version = "0.25.0"
+  context = var.context
+  name    = "delete-course"
+}
+
 module "label_save_course" {
   source = "cloudposse/label/null"
   # Cloud Posse recommends pinning every module to a specific version
@@ -136,6 +152,58 @@ module "lambda_save_course" {
     dynamodb = {
       effect    = "Allow",
       actions   = ["dynamodb:PutItem"],
+      resources = ["${var.courses_table_arn}"]
+    }
+  }
+
+  tags = module.label.tags
+}
+
+module "lambda_update_course" {
+  source        = "terraform-aws-modules/lambda/aws"
+  version       = "7.20.1"
+  function_name = module.label_update_course.id
+  description   = "My awesome lambda function"
+  handler       = "index.handler"
+  runtime       = "nodejs16.x"
+
+  source_path = "${path.module}/src/update-course"
+
+  environment_variables = {
+    TABLE_COURSES = var.courses_table
+  }
+  attach_policy_statements = true
+
+  policy_statements = {
+    dynamodb = {
+      effect    = "Allow",
+      actions   = ["dynamodb:PutItem"],
+      resources = ["${var.courses_table_arn}"]
+    }
+  }
+
+  tags = module.label.tags
+}
+
+module "lambda_delete_course" {
+  source        = "terraform-aws-modules/lambda/aws"
+  version       = "7.20.1"
+  function_name = module.label_delete_course.id
+  description   = "My awesome lambda function"
+  handler       = "index.handler"
+  runtime       = "nodejs16.x"
+
+  source_path = "${path.module}/src/delete-course"
+
+  environment_variables = {
+    TABLE_COURSES = var.courses_table
+  }
+  attach_policy_statements = true
+
+  policy_statements = {
+    dynamodb = {
+      effect    = "Allow",
+      actions   = ["dynamodb:DeleteItem"],
       resources = ["${var.courses_table_arn}"]
     }
   }
